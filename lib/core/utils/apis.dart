a4e1fed858845978253a22ac22e454dafc51f964
Hoàng <hoanghung20030@gmail.com>
2025-03-16 16:11:26 +0700

    fix bug ui register

--- lib/core/utils/apis.dart
+++ lib/core/utils/apis.dart
@@ -12,6 +12,59 @@ class ApiService {
 
   String? token = "";
 
+  // Future<http.Response> request({
+  //   required String path,
+  //   required String method,
+  //   required String typeUrl,
+  //   required String currentPath,
+  //   Map<String, dynamic>? data,
+  // }) async {
+  //   Uri url;
+  //   if (typeUrl == UrlConstant().baseUrl) {
+  //     if (currentPath != "/login" && currentPath != "/sign-up") {
+  //       token = await AuthService().getToken();
+  //     }
+  //     url = Uri.parse('$baseUrl$path');
+  //   } else if (typeUrl == UrlConstant().locationUrl) {
+  //     token = "";
+  //     url = Uri.parse('$locationUrl$path');
+  //   } else {
+  //     throw Exception('Invalid URL type: $typeUrl');
+  //   }
+  //
+  //   http.Response response;
+  //
+  //   switch (method.toUpperCase()) {
+  //     case 'GET':
+  //       response = await http.get(url, headers: _defaultHeaders(token!));
+  //       break;
+  //     case 'POST':
+  //       response = await http.post(url,
+  //           headers: _defaultHeaders(token!), body: jsonEncode(data));
+  //       break;
+  //     case 'PUT':
+  //       response = await http.put(url,
+  //           headers: _defaultHeaders(token!), body: jsonEncode(data));
+  //       break;
+  //     case 'PATCH':
+  //       response = await http.patch(url,
+  //           headers: _defaultHeaders(token!), body: jsonEncode(data));
+  //       break;
+  //     case 'DELETE':
+  //       response = await http.delete(url,
+  //           headers: _defaultHeaders(token!), body: jsonEncode(data));
+  //       break;
+  //     default:
+  //       throw Exception('Unsupported HTTP method: $method');
+  //   }
+  //   print(token);
+  //   if (response.statusCode >= 200 && response.statusCode < 300) {
+  //     return response;
+  //   } else {
+  //     throw Exception('Failed to load data: ${response.statusCode}');
+  //   }
+  // }
+
   Future<http.Response> request({
     required String path,
     required String method,
@@ -39,32 +92,26 @@ class ApiService {
         response = await http.get(url, headers: _defaultHeaders(token!));
         break;
       case 'POST':
-        response = await http.post(url,
-            headers: _defaultHeaders(token!), body: jsonEncode(data));
+        response = await http.post(url, headers: _defaultHeaders(token!), body: jsonEncode(data));
         break;
       case 'PUT':
-        response = await http.put(url,
-            headers: _defaultHeaders(token!), body: jsonEncode(data));
+        response = await http.put(url, headers: _defaultHeaders(token!), body: jsonEncode(data));
         break;
       case 'PATCH':
-        response = await http.patch(url,
-            headers: _defaultHeaders(token!), body: jsonEncode(data));
+        response = await http.patch(url, headers: _defaultHeaders(token!), body: jsonEncode(data));
         break;
       case 'DELETE':
-        response = await http.delete(url,
-            headers: _defaultHeaders(token!), body: jsonEncode(data));
+        response = await http.delete(url, headers: _defaultHeaders(token!), body: jsonEncode(data));
         break;
       default:
         throw Exception('Unsupported HTTP method: $method');
     }
-    print(token);
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      return response;
-    } else {
-      throw Exception('Failed to load data: ${response.statusCode}');
-    }
-  }
 
+    print(token);
+
+    // Trả về phản hồi ngay cả khi gặp lỗi
+    return response;
+  }
   Map<String, String> _defaultHeaders(String token) {
     return {
       'Content-Type': 'application/json',
